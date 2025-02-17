b77e3ceea5dcfc28a3137ab742ef13d1dbea4c86
prasad.bhor <bhorp2003@gmail.com>
2025-02-17 16:07:59 +0530

    greet message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [greet, setGreet] = useState('Hello World');
+    const changeGreet = () => 
+        {setGreet('Hello Prasad');}
+    return (
+        <div>
+            <button onClick={changeGreet}>Change the Greet Message</button>
+            <h1>{greet}</h1>
+        </div>
+    );
 }
 
 export default App;
