9e8df09dfa4832bbd3c5dbf24367948d5bba385b
prasad.bhor <bhorp2003@gmail.com>
2025-02-17 17:11:13 +0530

    factorial no. program

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useState } from 'react';
 
-function App() {
-    const [greet, setGreet] = useState('Hello World');
-    const changeGreet = () => 
-        {setGreet('Hello Prasad');}
-    return (
-        <div>
-            <button onClick={changeGreet}>Change the Greet Message</button>
-            <h1>{greet}</h1>
-        </div>
+function App(){
+    const [number, setNumber]=useState("");
+    const [factorial, setFactorial] = useState("");
+
+    function calculateFactorial() {
+        let fact = 1;
+        for(let i=1;i<=number;i++){
+            fact*=i
+        }
+        setFactorial(fact)
+    };
+ 
+    return(
+        <>
+            <input type="number" value={number} onChange={(e)=>{setNumber(e.target.value)}}/>
+            <button onClick={calculateFactorial}>Submit</button>
+            {factorial !== "" && <p>Factorial: {factorial}</p>}
+        </>
     );
 }
-
 export default App;
