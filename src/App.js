07d20ace774cafca5d139e56c19022fe240be838
prasad.bhor <bhorp2003@gmail.com>
2025-02-20 12:56:00 +0530

    even odd program

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
 import React, { useState } from 'react';
 
-function App(){
-    const [number, setNumber]=useState("");
-    const [factorial, setFactorial] = useState("");
+function App() {
+    const [input, setInput] = useState('');
+    const [result, setResult] = useState([]);
+    const [even, setEven] = useState([]);
+    const [odd, setOdd] = useState([])
 
-    function calculateFactorial() {
-        let fact = 1;
-        for(let i=1;i<=number;i++){
-            fact*=i
+    function EvenOdd(event) {
+        const value = event.target.value;
+        setInput(value);
+    }
+
+    function calculate() {
+        if (input % 2 === 0) {
+            setResult([...result, input]);
+        } else {
+            setResult(...odd, input);
         }
-        setFactorial(fact)
-    };
- 
-    return(
-        <>
-            <input type="number" value={number} onChange={(e)=>{setNumber(e.target.value)}}/>
-            <button onClick={calculateFactorial}>Submit</button>
-            {factorial !== "" && <p>Factorial: {factorial}</p>}
-        </>
-    );
+    }
+        function OddEven() {
+            if (input % 2 === 0) {
+                setEven([...even, input]);
+            } else {
+                setOdd([...odd, input]);
+            }
+        
+    }
+    return (
+        <div>
+            <label>Enter Number :</label>
+            <input type="number" value={input} onChange={EvenOdd} />
+                <button onClick={calculate}>Submit</button><br />
+            <div>{result}</div>
+            <div>
+                <button onClick={OddEven}>Calculate</button><br />
+                <label>Even Numbers:</label>
+                {even.map((num, index) => (
+                    <div key={index}>{num}</div>
+                ))}
+
+            </div>
+            <div>
+                <label>Odd Numbers:</label>
+                {odd.map((num, index) => (
+                    <div key={index}>{num}</div>
+                ))}
+            </div>
+        </div>
+    )
 }
 export default App;
